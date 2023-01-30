7ea6e73f65e29dd1cde31c2aefab195c7c83e293
jeraxProg <113060248+jeraxProg@users.noreply.github.com>
2023-01-31 05:10:38 +0800

    opt

--- components/HomePage/index.js
+++ components/HomePage/index.js
@@ -1,6 +1,7 @@
 import TitleSection from './TitleSection'
 import BrowseByCategory from './BrowseByCategory';
 import JobsofDay from './JobsofDay';
+import Ads from './Ads';
 
 
 const HomePage = ({employers, cat_list, cat_length}) => {
@@ -8,7 +9,7 @@ const HomePage = ({employers, cat_list, cat_length}) => {
     <>
     
       <TitleSection cat_list={cat_list} employers={employers} />
-
+      <Ads />
       <BrowseByCategory cat_list={cat_list} cat_length={cat_length} />
       <JobsofDay employers={employers} cat_list={cat_list} />
     </>
